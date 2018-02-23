662a6901a374e8ac848e602f9be2663b206139e1
AnnieK7H <anjahalatyan@gmail.com>
2018-02-23 16:05:59 +0200

    first commit

--- src/test/java/stepDefinitions/SampleSteps.java
+++ src/test/java/stepDefinitions/SampleSteps.java
@@ -63,4 +63,19 @@ public class SampleSteps {
             driver.findElement(By.id(e.getKey())).sendKeys(e.getValue());
         }
     }
+
+    @And("^I should see home page description$")
+    public void iShouldSeeHomePageDescription() throws Throwable {
+       assertEquals("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", driver.findElement(By.cssSelector("p")).getText());
+    }
+
+    @When("^I am on page locator$")
+    public void iAmOnPageLocator() throws Throwable {
+        driver.get("https://kristinek.github.io/test-sample/examples/loc");
+    }
+
+    @Then("^I should see Heading (\\d+)$")
+    public void iShouldSeeHeading(int arg0) throws Throwable {
+        assertEquals("Heading 1", driver.findElement(By.id("heading_1")).getText());
+    }
 }
